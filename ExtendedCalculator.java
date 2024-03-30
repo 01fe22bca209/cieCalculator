37bb917601f0f5705794637832f18749c637f329
Keerthi <01fe22bca209@kletech.ac.in>
2024-03-30 23:01:02 +0530

    code related to basic arithmetic operations including square and cube

---
+++ ExtendedCalculator.java
@@ -0,0 +1,8 @@
+class ExtendedCalculator{
+    float square(float operand_1){
+        return(operand_1*operand_1);
+    }
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+}
